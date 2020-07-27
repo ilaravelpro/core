92c0d8437896bc78d50f17aebdf0d0e1bd9ef946
Amir Hossein Jahani <me@iamir.net>
2020-07-27 09:12:17 +0430

    add _getBearingGeo

--- src/helpers/measuring.php
+++ src/helpers/measuring.php
@@ -1,6 +1,7 @@
 <?php
 
-function _getDistanceGeo($c1, $c2, $opt_radius = 6371008.8) {
+function _getDistanceGeo($c1, $c2, $opt_radius = 6371008.8)
+{
     $radius = $opt_radius;
     $lat1 = deg2rad($c1[1]);
     $lat2 = deg2rad($c2[1]);
@@ -12,6 +13,20 @@ function _getDistanceGeo($c1, $c2, $opt_radius = 6371008.8) {
     return 2 * $radius * atan2(sqrt($a), sqrt(1 - $a));
 }
 
-function _toNM($number) {
-    return round($number  * 0.000539956803, 2);
+function _getBearingGeo($lonlat1, $lonlat2)
+{
+    $p1 = $lonlat1[1] * pi() / 180;
+    $p2 = $lonlat2[1] * pi() / 180;
+    $a1 = $lonlat1[0] * pi() / 180;
+    $a2 = $lonlat2[0] * pi() / 180;
+    $y = sin($a2 - $a1) * cos($p2);
+    $x = cos($p1) * sin($p2) - sin($p1) * cos($p2) * cos($a2 - $a1);
+    $result = (atan2($y, $x) * 180 / pi() + 360) / 360;
+    return round(($result - (int) $result) * 360);
 }
+
+function _toNM($number)
+{
+    return round($number * 0.000539956803, 2);
+}
+
