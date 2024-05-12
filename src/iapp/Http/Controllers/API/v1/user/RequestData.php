50240345e6558930aaba87ecfc83d0db7e86a38e
Amirhossein Jahani <me@iamir.net>
2024-05-12 13:31:11 +0330

    Update RequestData.php

--- src/iapp/Http/Controllers/API/v1/user/RequestData.php
+++ src/iapp/Http/Controllers/API/v1/user/RequestData.php
@@ -1,8 +1,14 @@
 <?php
 
-namespace iLaravel\Core\iapp\Http\Controllers\API\v1\user;
+namespace iLaravel\Core\iApp\Http\Controllers\API\v1\User;
+
+use iLaravel\Core\iApp\Http\Requests\iLaravel as Request;
 
 trait RequestData
 {
-
+    public function requestData(Request $request, $action, &$data)
+    {
+        if (in_array($action, ['store']))
+            $data['creator_id'] = auth()->id();
+    }
 }
