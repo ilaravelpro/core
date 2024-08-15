8387caafc9821a2535bb962764cc3a22d5f24645
iAmir <me@iamir.net>
2024-08-15 07:07:30 +0330

    up

--- src/iapp/Http/Middleware/ApiIfAuthenticated.php
+++ src/iapp/Http/Middleware/ApiIfAuthenticated.php
@@ -16,7 +16,7 @@ class ApiIfAuthenticated
      */
     public function handle($request, Closure $next)
     {
-        if (Auth::guard('api')->check() || (!Auth::guard('api')->check() && !in_array(\Route::currentRouteName(), iconfig('ilaravel.authIf.routes', [])))) {
+        if (Auth::guard('api')->check() || (!Auth::guard('api')->check() && !in_array(\Route::currentRouteName(), iconfig('authIf.routes', [])))) {
             return app(iAuthenticate::class)->handle($request, $next, 'api');
         }
         return $next($request);
