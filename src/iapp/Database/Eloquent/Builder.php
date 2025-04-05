69993bd95df4e124db99c0983cc0ab41c4ca9821
Amirhossein Jahani <me@iamir.net>
2025-04-05 20:07:05 +0330

    Update Builder.php

--- src/iapp/Database/Eloquent/Builder.php
+++ src/iapp/Database/Eloquent/Builder.php
@@ -8,8 +8,8 @@ use Illuminate\Support\Facades\Cache;
 class Builder extends EloquentBuilder
 {
     protected $cacheEnabled = true;
-    protected $cacheTtl = 60;
-    protected $cacheKey = null;
+    public $cacheTtl = 60;
+    public $cacheKey = null;
 
     public function enableCache($ttl = 60, $key = null)
     {
@@ -24,23 +24,35 @@ class Builder extends EloquentBuilder
         $this->cacheEnabled = false;
         return $this;
     }
-    protected function runSelect()
+    public function get($columns = ['*'])
     {
         if (!$this->cacheEnabled)
-            return parent::runSelect();
+            return parent::get($columns);
         $cacheKey = $this->cacheKey ?? $this->generateCacheKey();
-        dd($cacheKey);
-        return unserialize(Cache::remember($cacheKey, $this->cacheTtl, function () use ($columns) {
-            return  serialize(parent::runSelect());
+        return unserialize(Cache::remember($cacheKey . ":get", $this->cacheTtl, function () use ($columns, $cacheKey) {
+            $result = parent::get($columns);
+            $result->cacheKey = $cacheKey . ":get";
+            return serialize($result);
         }));
     }
 
+    public function paginate($perPage = null, $columns = ['*'], $pageName = 'page', $page = null)
+    {
+        if (!$this->cacheEnabled)
+            return parent::paginate($perPage, $columns, $pageName, $page);
+        $cacheKey = $this->cacheKey ?? $this->generateCacheKey();
+        return unserialize(Cache::remember($cacheKey . ":paginate", $this->cacheTtl, function () use ($cacheKey, $perPage, $columns, $pageName, $page) {
+            $result = parent::paginate($perPage, $columns, $pageName, $page);
+            $result->cacheKey = $cacheKey . ":paginate";
+            return serialize($result);
+        }));
+    }
 
     protected function generateCacheKey()
     {
         $query = $this->getQuery();
         $sql = $query->toSql();
         $bindings = $query->getBindings();
-        return "ilaravel:db:{$this->getModel()->getTable()}" . md5( $sql . serialize($bindings));
+        return "ilaravel:db:{$this->getModel()->getTable()}:" . md5( $sql . serialize($bindings));
     }
 }
